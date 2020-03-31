fef06988a70e4a1b42e0424d69bf90b170c7f4ae
Bilqist <bilqistaisaah@gmail.com>
2020-03-31 21:18:40 +0700

    Berhasil Hapus Data

--- controller.js
+++ controller.js
@@ -58,12 +58,25 @@ exports.ubahMahasiswa = function (req, res) {
     var nama = req.body.nama;
     var jurusan = req.body.jurusan;
 
-    connection.query('UPDATE mahasiswa SET nim=?,nama=?,jurusan=? WHERE id_mahasiswa=?', [nim, nama, jurusan,id],
+    connection.query('UPDATE mahasiswa SET nim=?,nama=?,jurusan=? WHERE id_mahasiswa=?', [nim, nama, jurusan, id],
         function (error, rows, fields) {
             if (error) {
                 console.log(error);
             } else {
                 response.ok("Berhasil Ubah data!", res);
+            }
+        });
+};
+
+//menghapus mahasiswa berdasarkan id
+exports.hapusMahasiswa = function (req,res) {
+    var id = req.body.id_mahasiswa;
+    connection.query('DELETE FROM mahasiswa WHERE id_mahasiswa=?', [id],
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok("Berhasil Hapus data!",res);
             }
         });
 };
